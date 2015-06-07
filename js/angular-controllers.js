3e76a4cd061b535e2be94792942cab5df6b06f3d
Vincent Kwok <manchunman0720@gmail.com>
2015-06-08 01:51:22 +0800

    refine using angular

--- js/angular-controllers.js
+++ js/angular-controllers.js
@@ -1,5 +1,16 @@
 var myApp = angular.module('myApp', []);
 
+function isValidPassword(password) {
+  var reChar = /[A-Z]/;
+  var reDigit = /\d/;
+  return reChar.test(password) && reDigit.test(password);
+};
+
+var isEmail = function (email) {
+  var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+  return re.test(email);
+};
+
 myApp.controller('ContactFormController', function ($scope) {
     $scope.contactFormNumber = 1;
     $scope.contactForms = [$scope.contactFormNumber];
@@ -33,4 +44,18 @@ myApp.controller('EditCompanyFormController', function ($scope) {
         socialNetworkNumber++;
         $scope.forms.push(socialNetworkNumber);
     };
+});
+
+myApp.controller('PasswordFormController', function ($scope) {
+    $scope.isValidEmail = true;
+    $scope.isValidNewEmail = true;
+    $scope.isValidPassword = true;
+
+    $scope.checkEmail = function (email) {
+        return isEmail(email);
+    };
+
+    $scope.checkPassword = function (password) {
+        return isValidPassword(password);
+    }
 });
